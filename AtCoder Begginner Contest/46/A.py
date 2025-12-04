d5b0e5091138c04d6b4064db935534cd5e2b3f10
Shin-ye-jin <fx0605@naver.com>
2025-12-04 22:13:57 +0900

    46 - 1

---
+++ AtCoder Begginner Contest/46/A.py
@@ -0,0 +1,10 @@
+# AtCoDeer and Paint Cans
+matrix= list(map(int,input().split()))
+
+result = set(matrix)
+
+print(len(result))
+
+# 수를 리스트로 입력받아서 저장한다.
+# set 함수를 사용하여 중복을 제거한다.
+# len 함수를 사용하여 길이를 구하면서 개수로 출력
---
+++ AtCoder Begginner Contest/46/B.py
@@ -0,0 +1,4 @@
+# Painting Balls with AtCoDeer
+# 공의 개수 N
+# 페인트의 개수 K
+# 중복 없이 공의 개수만큼 칠할 수 있는 경우의 수라고 생각
