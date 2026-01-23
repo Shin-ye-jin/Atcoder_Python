23f3dd730c2fa29f1e30b907860b741accd29412
Shin-ye-jin <fx0605@naver.com>
2026-01-23 22:49:29 +0900

    49 - 1

---
+++ AtCoder Begginner Contest/49/A.py
@@ -0,0 +1,8 @@
+# A - UOIAUAI
+
+a = input()
+
+if a == 'a' or a == 'e' or a =='i' or a == 'o' or a=='u':
+    print('vowel')
+else:
+    print('consonant')
---
+++ AtCoder Begginner Contest/49/B.py
@@ -0,0 +1,9 @@
+# B - Thin
+
+h,w = map(int,input().split())
+# matrix = list()
+
+for i in range(h):
+    matrix = input()
+    for j in range(2):
+        print(matrix)
