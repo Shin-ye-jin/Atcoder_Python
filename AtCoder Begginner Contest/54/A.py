7ca09516ad904ecd0cf4dfe04dbc02741ff0661e
Shin-ye-jin <fx0605@naver.com>
2026-03-03 20:31:52 +0900

    54 - 1

---
+++ AtCoder Begginner Contest/54/A.py
@@ -0,0 +1,14 @@
+# A - One Card Poker
+
+a,b = map(int,input().split())
+
+if a == 13 and b == 1:
+    print("Bob")
+elif a == 1 and b == 13:
+    print("Alice")
+elif a == b:
+    print("Draw")
+elif a > b:
+    print("Alice")
+elif a < b:
+    print("Bob")
