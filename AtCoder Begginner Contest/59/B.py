1dc0d32e996691156f00ddbb50c8412df05a38d1
Shin-ye-jin <fx0605@naver.com>
2026-04-02 00:10:41 +0900

    59 - 2

---
+++ AtCoder Begginner Contest/59/B.py
@@ -0,0 +1,12 @@
+# B - Comparison
+import sys
+
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+
+if a>b:
+    print("GREATER")
+elif a<b:
+    print("LESS")
+else:
+    print("EQUAL")
