d81ce6c0e27b9e44ea3ac212375497ae9ef8360b
Shin-ye-jin <fx0605@naver.com>
2026-09-13 12:16:10 +0900

    SW Level 2 - 1일차 완료

--- SW Expert Academy/Level 2/1859.py
+++ SW Expert Academy/Level 2/1859.py
@@ -1,6 +1,20 @@
 # 1859 백만 장자 프로젝트
 
-n = int(input())
 # 제일 뒤에 있는 숫자를 가장 비싼 매도가로 지정
 # 역순으로 하나씩 비교하면서 오늘 매매가가 가장 비싼 매도가보다 작다면 시세차익이 생김
 # 오늘 매매가가 더 크다면 가장 비싼 매도가가 해당 매매가로 변경됨.
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+for i in range(n):
+    m = int(sys.stdin.readline().strip())
+    matrix = list(map(int, sys.stdin.readline().split()))
+    max_price = matrix[-1]
+    total = 0
+    for j in range(m-1,-1,-1):
+        if max_price > matrix[j]:
+            total = total + max_price-matrix[j]
+        elif max_price < matrix[j]:
+            max_price = matrix[j]
+    print(f"#{i+1} {total}")
